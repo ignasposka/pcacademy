f4de9a3be37496a6d6f913e4f58c13b8f7ff36dc
Ignas <ignasp789@gmail.com>
2018-11-10 23:09:53 +0200

    Implemented reset on empty. Chosen more appropriate event.

--- public/index.js
+++ public/index.js
@@ -6,7 +6,11 @@ import pxToEm from 'px-to-em';
 
     const textarea = document.getElementsByTagName('textarea')[0];
 
-    const resize = () => {         
+    const resize = () => {    
+        if(!textarea.value) {
+            document.documentElement.style.setProperty('--row-count', `${minRows}em`);
+            return;
+        }
         const scrollHeight = textarea.scrollHeight;
         const diff = scrollHeight - textarea.clientHeight;
         const rowCount = pxToEm(scrollHeight, textarea);
@@ -16,6 +20,6 @@ import pxToEm from 'px-to-em';
     }
 
     document.documentElement.style.setProperty('--row-count', `${minRows}em`);
-    textarea.addEventListener('keypress', resize);
     textarea.addEventListener('paste', resize);
+    textarea.addEventListener('keyup', resize);
 })()
