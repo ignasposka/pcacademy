0c50d823dca6d592da05419c73f3e383e4946d33
Ignas <ignasp789@gmail.com>
2018-11-10 21:59:12 +0200

    Implemented textarea growing limitation.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import pxToEm from 'px-to-em';
 const minRows = 5;
 const maxRows = 7;
 
 (()=> {
     const resize = () => {
         const textarea = document.getElementsByTagName('textarea')[0];   
-        const diff = textarea.scrollHeight - textarea.clientHeight;
-        if(diff > 0){
-           document.documentElement.style.setProperty('--row-count', `${textarea.scrollHeight}px`);
+        const scrollHeight = textarea.scrollHeight;
+        const diff = scrollHeight - textarea.clientHeight;
+        const rowCount = pxToEm(scrollHeight, textarea);
+        if(diff > 0 && rowCount <= maxRows){
+           document.documentElement.style.setProperty('--row-count', `${scrollHeight}px`);
         }
     }
 
--- public/bundle.js
+++ public/bundle.js
@@ -90,10 +90,23 @@
 /*!******************!*\
   !*** ./index.js ***!
   \******************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("var minRows = 5;\nvar maxRows = 7;\n\n(function () {\n  var resize = function resize() {\n    var textarea = document.getElementsByTagName('textarea')[0];\n    var diff = textarea.scrollHeight - textarea.clientHeight;\n\n    if (diff > 0) {\n      document.documentElement.style.setProperty('--row-count', \"\".concat(textarea.scrollHeight, \"px\"));\n    }\n  };\n\n  var textarea = document.getElementsByTagName('textarea')[0];\n  textarea.addEventListener('keyup', resize);\n})();\n\n//# sourceURL=webpack:///./index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var px_to_em__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! px-to-em */ \"./node_modules/px-to-em/pxToEm.mjs\");\n\nvar minRows = 5;\nvar maxRows = 7;\n\n(function () {\n  var resize = function resize() {\n    var textarea = document.getElementsByTagName('textarea')[0];\n    var scrollHeight = textarea.scrollHeight;\n    var diff = scrollHeight - textarea.clientHeight;\n    var rowCount = Object(px_to_em__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(scrollHeight, textarea);\n\n    if (diff > 0 && rowCount <= maxRows) {\n      document.documentElement.style.setProperty('--row-count', \"\".concat(scrollHeight, \"px\"));\n    }\n  };\n\n  var textarea = document.getElementsByTagName('textarea')[0];\n  textarea.addEventListener('keyup', resize);\n})();\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ }),
+
+/***/ "./node_modules/px-to-em/pxToEm.mjs":
+/*!******************************************!*\
+  !*** ./node_modules/px-to-em/pxToEm.mjs ***!
+  \******************************************/
+/*! exports provided: default */
+/***/ (function(__webpack_module__, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return pxToEm; });\n// Copyright © 2017 Jackson Ray Hamilton\n\n// Permission is hereby granted, free of charge, to any person obtaining a copy\n// of this software and associated documentation files (the “Software”), to deal\n// in the Software without restriction, including without limitation the rights\n// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n// copies of the Software, and to permit persons to whom the Software is\n// furnished to do so, subject to the following conditions:\n\n// The above copyright notice and this permission notice shall be included in\n// all copies or substantial portions of the Software.\n\n// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE\n// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n// SOFTWARE.\n\nfunction isNumber (value) {\n  return value && typeof value === 'number';\n}\n\nfunction isBottomValue (value) {\n  return value === null || value === undefined;\n}\n\nfunction isObjectLike (value) {\n  return !isBottomValue(value) && typeof value === 'object';\n}\n\nfunction getPrototype (value) {\n  if (isBottomValue(value)) {\n    return value;\n  }\n  return Object.getPrototypeOf(value);\n}\n\nfunction getSource (func) {\n  return Function.prototype.toString.call(func);\n}\n\nfunction isPlainObject (value) {\n  if (!isObjectLike(value)) {\n    return false;\n  }\n  var prototype = getPrototype(value);\n  if (!isObjectLike(prototype)) {\n    return false;\n  }\n  var Constructor = prototype.constructor;\n  return typeof Constructor === 'function' &&\n    Constructor instanceof Constructor &&\n    getSource(Constructor) === getSource(Object);\n}\n\nfunction isElement (value) {\n  return isObjectLike(value) && value.nodeType === 1 && !isPlainObject(value);\n}\n\nfunction getPxFontSize (element) {\n  return parseFloat(getComputedStyle(element).fontSize);\n}\n\nfunction pxToEm (px, element) {\n  if (!isNumber(px)) {\n    throw new TypeError('px is not a number');\n  }\n  element = isBottomValue(element) ? document.documentElement : element;\n  if (!isElement(element)) {\n    throw new TypeError('element is not an element');\n  }\n  var temporaryElement = document.createElement('div');\n  temporaryElement.style.setProperty('position', 'absolute', 'important');\n  temporaryElement.style.setProperty('visibility', 'hidden', 'important');\n  temporaryElement.style.setProperty('font-size', '1em', 'important');\n  element.appendChild(temporaryElement);\n  var baseFontSize = getPxFontSize(temporaryElement);\n  temporaryElement.parentNode.removeChild(temporaryElement);\n  return px / baseFontSize;\n}\n\n\n//# sourceURL=webpack:///./node_modules/px-to-em/pxToEm.mjs?");
 
 /***/ })
 
