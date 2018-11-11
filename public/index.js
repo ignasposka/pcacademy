fd2948ebd87c9830dd09224e42b0a0698d816a5a
Ignas <ignasp789@gmail.com>
2018-11-11 21:20:11 +0200

    Configured ESLint.

--- public/index.js
+++ public/index.js
@@ -1,21 +1,21 @@
 import pxToEm from 'px-to-em';
 
-(()=> {
+(() => {
     const minRows = 5;
     const maxRows = 7;
 
     const textarea = document.getElementsByTagName('textarea')[0];
 
-    const resize = () => {         
-        const scrollHeight = textarea.scrollHeight;
+    const resize = () => {
+        const { scrollHeight } = textarea;
         const diff = scrollHeight - textarea.clientHeight;
         const rowCount = pxToEm(scrollHeight, textarea);
-        if(diff > 0 && rowCount <= maxRows){
-           document.documentElement.style.setProperty('--row-count', `${scrollHeight}px`);
+        if (diff > 0 && rowCount <= maxRows) {
+            document.documentElement.style.setProperty('--row-count', `${scrollHeight}px`);
         }
-    }
+    };
 
     document.documentElement.style.setProperty('--row-count', `${minRows}em`);
     textarea.addEventListener('keypress', resize);
     textarea.addEventListener('paste', resize);
-})()
+})();
