b363e9a0e1be6ac55b0ae2b2affcd23ad4346c3c
Ignas <ignasp789@gmail.com>
2018-12-16 22:50:16 +0200

    Added credentials for ability to run tests.

--- API/Codebase/API/test/loadToken.js
+++ API/Codebase/API/test/loadToken.js
@@ -5,7 +5,7 @@ module.exports = (callback) => {
         method: 'POST',
         url: 'https://ignasposka.eu.auth0.com/oauth/token',
         headers: { 'content-type': 'application/json' },
-        body: '{"client_id":"","client_secret":"","audience":"space-saver","grant_type":"client_credentials"}'
+        body: '{"client_id":"H5Rbt2GpNuQQ8HMfGvgrQJdXn00dTx5J","client_secret":"EAPK8b3NJGXnrKh4ujNYh8vXKcMvLERdlwgR046bFYiMZaaRYGJ0JVgPHNauGv01","audience":"space-saver","grant_type":"client_credentials"}'
     };
 
     return new Promise((resolve, reject) => {
