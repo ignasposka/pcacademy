61f85702a7f037305e70259cd000ffc610a1dee3
Ignas <ignasp789@gmail.com>
2018-12-26 17:16:32 +0200

    Moving test execution to main.js to maintain executing order.

---
+++ API/Codebase/API/test/albums/usual/index.js
@@ -0,0 +1,4 @@
+exports.get = require('./get');
+exports.post = require('./post');
+exports.patch = require('./patch');
+exports.delete = require('./delete');
---
+++ API/Codebase/API/test/main.js
@@ -0,0 +1,47 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const { expect } = require('chai');
+const chaiHtpp = require('chai-http');
+const AlbumModel = require('../models/album');
+const mlog = require('mocha-logger');
+const loadToken = require('./loadToken');
+const albumsUsual = require('./albums/usual');
+const albumsIncorrect = require('./albums/incorrect_data');
+const mediaItemsUsual = require('./mediaItems/usual');
+const rimraf = require('rimraf');
+
+const apiUrl = 'http://localhost:8080';
+require('../index.js');
+
+const testAlbumsUsual = () => {
+    albumsUsual.post(apiUrl);
+    albumsUsual.patch(apiUrl);
+    albumsUsual.get(apiUrl);
+};
+
+const testAlbumsIncorrect = () => {
+    albumsIncorrect.post(apiUrl);
+    albumsIncorrect.patch(apiUrl);
+    albumsIncorrect.get(apiUrl);
+};
+
+const testMediaItemsUsual = () => {
+    mediaItemsUsual.post(apiUrl);
+};
+
+chai.should();
+chai.use(chaiHtpp);
+loadToken().then(() => {
+    describe('Albums', () => {
+        before((done) => {
+            AlbumModel.deleteMany({}, (err) => {
+                rimraf('uploads/*', () => done());
+            });
+        });
+
+        testAlbumsUsual();
+        // testAlbumsIncorrect();
+        testMediaItemsUsual();
+    });
+});
