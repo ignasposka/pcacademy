5b29a51dc085db3069d35f06fb231b0d2c1cbc00
Ignas <ignasp789@gmail.com>
2019-01-13 18:23:49 +0200

    Implemented get random photo endpoint.

--- API/Codebase/API/controllers/randomPhoto.js
+++ API/Codebase/API/controllers/randomPhoto.js
@@ -1,15 +1,21 @@
 const fetch = require('node-fetch');
 
 exports.get = (req, res, next) => {
-    fetch('https://api.pexels.com/v1/curated?per_page=1&page=1', {
+    fetch(`https://api.pexels.com/v1/curated?per_page=1&page=${getRandomInt()}`, {
         method: 'get',
         headers: { Authorization: process.env.PEXELS_API_KEY }
     })
         .then((response) => response.json())
         .then((data) => {
-            fetch(data.photos[0].src.large).then((photo) => {
-                res.status(200).send(photo);
+            fetch(data.photos[0].src.large).then(({ body }) => {
+                body.pipe(res);
             });
         })
         .catch((err) => next(err));
 };
+
+function getRandomInt() {
+    const min = 1;
+    const max = 50;
+    return Math.floor(Math.random() * (max - min)) + min;
+}
