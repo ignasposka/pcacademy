f9117cd5bf7ab0ac3a8d88882ca6670c4a13f0cc
Ignas <Ignasp789@gmail.com>
2019-01-21 21:05:00 +0200

    Moving to connect-react-router v5.

--- Front-end/app/reducers.js
+++ Front-end/app/reducers.js
@@ -5,19 +5,13 @@
 import { combineReducers } from 'redux-immutable';
 import { connectRouter } from 'connected-react-router/immutable';
 
-import history from 'utils/history';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
-export default function createReducer(injectedReducers = {}) {
-  const rootReducer = combineReducers({
-    language: languageProviderReducer,
-    ...injectedReducers
-  });
 
-  // Wrap the root reducer and return a new root reducer with router state
-  const mergeWithRouterState = connectRouter(history);
-  return mergeWithRouterState(rootReducer);
-}
+export default (history) => combineReducers({
+  language: languageProviderReducer,
+  router: connectRouter(history)
+})
